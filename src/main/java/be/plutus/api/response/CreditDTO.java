5d5a90eec309b614275b92afda55617afe14168d
Kristof Dewilde <me@krivi.be>
2016-07-05 22:45:30 +0200

    Merged API files from core project

---
+++ src/main/java/be/plutus/api/response/CreditDTO.java
@@ -0,0 +1,17 @@
+package be.plutus.api.response;
+
+public class CreditDTO{
+
+    private double amount;
+
+    public CreditDTO(){
+    }
+
+    public double getAmount(){
+        return amount;
+    }
+
+    public void setAmount( double amount ){
+        this.amount = amount;
+    }
+}
