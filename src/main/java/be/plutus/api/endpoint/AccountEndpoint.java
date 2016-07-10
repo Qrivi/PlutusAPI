6a9058fafa26f4a2e969110f93e6edb3db12ae2e
Kristof Dewilde <me@krivi.be>
2016-07-10 03:34:02 +0200

    Implemented all /account endpoints

--- src/main/java/be/plutus/api/endpoint/AccountEndpoint.java
+++ src/main/java/be/plutus/api/endpoint/AccountEndpoint.java
@@ -1,6 +1,8 @@
 package be.plutus.api.endpoint;
 
 import be.plutus.api.request.AccountCreateDTO;
+import be.plutus.api.request.AccountRemoveDTO;
+import be.plutus.api.request.AccountUpdateDTO;
 import be.plutus.api.response.AccountDTO;
 import be.plutus.api.response.Response;
 import be.plutus.api.response.meta.Meta;
@@ -33,9 +35,9 @@ public class AccountEndpoint{
     EndpointUtils endpointUtils;
 
     @RequestMapping( method = RequestMethod.GET )
-    public ResponseEntity<Response<Meta, AccountDTO>> get( Authentication authentication ){
+    public ResponseEntity<Response> get( Authentication authentication ){
 
-        Account account = accountService.getAccount( (String)authentication.getPrincipal() );
+        Account account = accountService.getAccount( (Integer)authentication.getPrincipal() );
 
         AccountDTO dto = new AccountDTO();
         dto.setEmail( account.getEmail() );
@@ -43,19 +45,38 @@ public class AccountEndpoint{
         dto.setCreated( account.getCreationDate() );
 
         return new ResponseEntity<>( new Response<>( Meta.success(), dto ), HttpStatus.OK );
-
     }
 
     @RequestMapping( method = RequestMethod.POST )
-    public ResponseEntity<Response> post( @Valid @RequestBody AccountCreateDTO dto , BindingResult result ){
+    public ResponseEntity<Response> post( @Valid @RequestBody AccountCreateDTO dto, BindingResult result ){
 
         if( result.hasErrors() )
             return endpointUtils.createErrorResponse( result );
 
-        accountService.createAccount( dto.getEmail(), dto.getPassword() );
+        accountService.createAccount( dto.getEmail(), dto.getPassword(), dto.getDefaultCurrency() );
 
-        return new ResponseEntity<>( new Response<>( Meta.success() ), HttpStatus.OK );
-
+        return new ResponseEntity<>( new Response<>( Meta.success() ), HttpStatus.CREATED );
     }
 
+    @RequestMapping( method = RequestMethod.PUT )
+    public ResponseEntity<Response> put( @Valid @RequestBody AccountUpdateDTO dto, BindingResult result, Authentication authentication ){
+
+        if( result.hasErrors() )
+            return endpointUtils.createErrorResponse( result );
+
+        accountService.updateAccount( (Integer)authentication.getPrincipal(), dto.getNewEmail(), dto.getNewPassword(), dto.getNewDefaultCurrency() );
+
+        return new ResponseEntity<>( new Response<>( Meta.success() ), HttpStatus.OK );
+    }
+
+    @RequestMapping( method = RequestMethod.DELETE )
+    public ResponseEntity<Response> delete( @Valid @RequestBody AccountRemoveDTO dto, BindingResult result, Authentication authentication ){
+
+        if( result.hasErrors() )
+            return endpointUtils.createErrorResponse( result );
+
+        accountService.removeAccount( (Integer)authentication.getPrincipal() );
+
+        return new ResponseEntity<>( new Response<>( Meta.success() ), HttpStatus.OK );
+    }
 }
